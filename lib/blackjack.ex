a3313c90263dbf1b42bd06fa968da6255a863b1c
stevemoxley <sdmfyc@gmail.com>
2021-01-30 00:14:05 -0600

    Changes

--- lib/blackjack.ex
+++ lib/blackjack.ex
@@ -3,21 +3,21 @@ defmodule Blackjack do
   def start do
     #Entry point for the game
 
-    #Get a dealer and a player
-    dealer = get_player(:dealer, 1000)
-    player = get_player(:human, 1000)
+    #Create a dealer and a player
+    dealer = create_player(:dealer, 1000)
+    player = create_player(:human, 1000)
 
     #Show the starting welcome message
     IO.puts("Welcome to Blackjack!")
 
     #start the first game
-    deal_new_hand(dealer, player)
+    play_new_hand(dealer, player)
 
     true
 
   end
 
-  def deal_new_hand(dealer, player) do
+  def play_new_hand(dealer, player) do
     deck = ["A", "K", "Q", "J", 10, 9, 8, 7, 6, 5, 4, 3, 2]
 
     #deal the dealer cards
@@ -33,19 +33,28 @@ defmodule Blackjack do
 
   def show_cards_and_prompt_for_decision(dealer, player) do
     #Show what the dealer is showing
-
-
-    IO.puts("Player cards:" <> Enum.join(player.cards, " "))
+    IO.puts("Dealer cards: *" <> Enum.join(Enum.slice(dealer.cards, 1, Enum.count(dealer.cards) - 1)))
+    IO.puts("Player cards: " <> Enum.join(player.cards, " "))
     IO.puts("Total: " <> to_string(get_hand_total(player.cards)))
-    IO.puts("What would you like to do? (H)it (S)tay") #Note, there are other options, those can be added later
+    decision = IO.gets("What would you like to do? (H)it (S)tay :\n")
+                          |> String.downcase()
+                          |> String.trim()
 
-    #Get the input from the player
-    #If they Hit. Add another card to their hand and the dealers hand
-    #If they stay. Play for the dealer and figure the outcome
+    case decision do
+      "h" ->
+        #If they Hit. Add another card to their hand and the dealers hand
+        IO.puts("Hit")
+      "s" ->
+        #If they stay. Play for the dealer and figure the outcome
+        IO.puts("Stay")
+       _ ->
+        IO.puts("Please select a valid option.")
+        show_cards_and_prompt_for_decision(dealer, player)
+    end
   end
 
   def get_hand_total(cards) do
-    aces = []
+    aces = [] #Split the aces out of the hand
     standard_cards_value = Enum.reduce(cards, 0, &(get_standard_card_value(&1) + &2))
     aces_value = Enum.reduce(aces, 0, &(get_ace_card_value(&1) + &2))
     standard_cards_value + aces_value
@@ -68,11 +77,12 @@ defmodule Blackjack do
     end
   end
 
+  @spec get_ace_card_value(any) :: 1
   def get_ace_card_value(hand_total) do
     1
   end
 
-  def get_player(type, starting_money) do
+  def create_player(type, starting_money) do
      %{type: type, money: starting_money, cards: []}
   end
 
