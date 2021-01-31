ff227cd14ac87f01d32b7838df17df1ec9709438
stevemoxley <sdmfyc@gmail.com>
2021-01-31 14:02:09 -0600

    Dealer play and rules, fix bug with has_blackjack, add hit function

--- lib/blackjack.ex
+++ lib/blackjack.ex
@@ -35,41 +35,63 @@ defmodule Blackjack do
     IO.puts("Player cards: " <> Enum.join(player.cards, " "))
     player_total = get_hand_total(player.cards)
     IO.puts("Total: " <> to_string(player_total))
-
-    #We have some checks to do here
-    #1) Did they hit black jack and the dealer didnt
-    if !has_blackjack(dealer.cards) && has_blackjack(player.cards) do
-      #We won!
-      #The round is over
-      true
-    end
-
-    #2) Did they bust?
-    if player_total > 21 do
-      #We lost
-      #The round is over
-      true
-    end
-
-    #Ask the player what they want to do
-    decision = IO.gets("What would you like to do? (H)it (S)tay :\n")
-                          |> String.downcase()
-                          |> String.trim()
-
-    case decision do
-      "h" ->
-        #If they Hit. Add another card to their hand and prompt again for decision
-        IO.puts("Hit")
-        player = %{player | cards: player.cards ++ [get_card()]}
-        show_cards_and_prompt_for_decision(dealer, player)
-      "s" ->
-        #If they stay. Play for the dealer and figure the outcome
-        IO.puts("Stay")
-       _ ->
-        IO.puts("Please select a valid option.")
-        show_cards_and_prompt_for_decision(dealer, player)
+    cond do
+      #1) Do we have blackjack and the dealer doesnt
+      !has_blackjack(dealer.cards) && has_blackjack(player.cards) ->
+        IO.puts("Blackjack")
+      #2) Did we bust?
+      player_total > 21 ->
+        IO.puts("Bust")
+      #3) None of the above just continue to player decision phase
+      true ->
+        #Ask the player what they want to do
+        decision = IO.gets("What would you like to do? (H)it (S)tay :\n")
+        |> String.downcase()
+        |> String.trim()
+        case decision do
+          "h" ->
+          #If they Hit. Add another card to their hand and prompt again for decision
+          IO.puts("Hit")
+          player = player |> hit()
+          show_cards_and_prompt_for_decision(dealer, player)
+          "s" ->
+          #If they stay. Play for the dealer and figure the outcome
+          IO.puts("Stay")
+          play_dealer(dealer, player)
+          _ ->
+          IO.puts("Please select a valid option.")
+          show_cards_and_prompt_for_decision(dealer, player)
+        end
     end
   end
+
+  def play_dealer(dealer, player) do
+    #The AI must either bust or hit 17 or above
+    dealer_total = dealer.cards |> get_hand_total()
+
+    if (dealer_total < 17) do
+      #Hit
+      dealer = dealer |> hit()
+      #Keep playing the dealer until we hit 17 or above or bust
+      play_dealer(dealer, player)
+    else
+      #stay
+      player_total = player.cards |> get_hand_total()
+
+      cond do
+        player_total > dealer_total ->
+          #win
+          IO.puts("Player wins")
+        player_total < dealer_total ->
+          #lose
+          IO.puts("Player loses")
+        player_total == dealer_total ->
+          #push
+          IO.puts("Push")
+      end
+    end
+  end
+
 
   def has_blackjack(cards) do
     has_ace = Enum.member?(cards, "A")
@@ -78,16 +100,18 @@ defmodule Blackjack do
             Enum.member?(cards, "J") ||
             Enum.member?(cards, 10)
 
-    has_ace && has_10
+    has_ace && has_10 && Enum.count(cards) == 2
   end
-
-
 
   def get_hand_total(cards) do
     number_of_aces = Enum.count(cards, &(&1 == "A")) #Count the number of aces
     standard_cards_value = Enum.reduce(cards, 0, &(get_standard_card_value(&1) + &2))
     aces_value = get_aces_value(standard_cards_value, number_of_aces)
     standard_cards_value + aces_value
+  end
+
+  def hit(player) do
+    %{player | cards: player.cards ++ [get_card()]}
   end
 
   def get_aces_value(standard_cards_value, number_of_aces) do
